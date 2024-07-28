30218ca940dea178a0bde28152b66b94ccb0f469
Galiona <my@aglebova.ru>
2024-07-28 21:33:46 +0500

    theme

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'main_page.dart';
 
+
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
 
@@ -15,129 +16,140 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: _isSignIn
-                ? [
-                    Theme.of(context).colorScheme.secondary,
-                    Theme.of(context).colorScheme.primary,
-                  ]
-                : [
-                    Theme.of(context).colorScheme.primary,
-                    Theme.of(context).colorScheme.secondary,
-                  ],
-            stops: const [0.1, 0.9], // 90% primary, 10% secondary
-          ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: SingleChildScrollView( // Wrap the Column with SingleChildScrollView
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                // Заголовок
-                Text(
-                  _isSignIn ? 'Вход' : 'Регистрация',
-                  style: const TextStyle(
-                    fontSize: 32,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white,
-                  ),
-                ),
-                const SizedBox(height: 30),
-
-                // Поле логина
-                TextField(
-                  decoration: InputDecoration(
-                    hintText: 'Почта',
-                    filled: true,
-                    fillColor: Colors.white.withOpacity(0.2), // Используем withOpacity
-                    border: const OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-
-                // Поле пароля
-                TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Пароль',
-                    filled: true,
-                    fillColor: Colors.white.withOpacity(0.2), // Используем withOpacity
-                    border: const OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.all(Radius.circular(20)),
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 20),
-
-                // Поле "Повторить пароль" (только для регистрации)
-                if (!_isSignIn)
-                  TextField(
-                    obscureText: true,
-                    decoration: InputDecoration(
-                      hintText: 'Повторить пароль',
-                      filled: true,
-                      fillColor: Colors.white.withOpacity(0.2), // Используем withOpacity
-                      border: const OutlineInputBorder(
-                        borderSide: BorderSide.none,
-                        borderRadius: BorderRadius.all(Radius.circular(20)),
-                      ),
-                    ),
-                  ),
-                const SizedBox(height: 30),
-
-                // Кнопка "Войти"
-                ElevatedButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(context, 
-                        MaterialPageRoute(
-                          builder: (context) => const MainPage()));
-                    // Обработка входа/регистрации
-                  },
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                    textStyle: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Color(0xFFff9033),
-                    ),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20),
-                    ),
-                  ),
-                  child: Text(_isSignIn ? 'Войти' : 'Зарегистрироваться'),
-                ),
-                const SizedBox(height: 20),
-
-                // Кнопка "У меня еще нет аккаунта"
-                TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _isSignIn = !_isSignIn;
-                    });
-                  },
-                  child: Text(
-                    _isSignIn
-                        ? 'У меня еще нет аккаунта'
-                        : 'Вернуться на страницу входа',
-                    style: const TextStyle(
-                      color: Colors.white,
-                    ),
-                  ),
-                ),
-              ],
+      body: Stack(
+        children: [
+          // Градиентный фон
+          Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: _isSignIn
+                    ? [
+                        Theme.of(context).colorScheme.secondary,
+                        Theme.of(context).colorScheme.primary,
+                      ]
+                    : [
+                        Theme.of(context).colorScheme.primary,
+                        Theme.of(context).colorScheme.secondary,
+                      ],
+                stops: const [0.1, 0.9], // 90% primary, 10% secondary
+              ),
             ),
           ),
-        ),
+          // Содержимое страницы
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    // Отступ сверху
+                    const SizedBox(height: 80), // Добавлен отступ
+
+                    // Заголовок приложения
+                    Text(
+                      'Менеджер задач',
+                      style: TextStyle(
+                        fontSize: 40, // Увеличили размер шрифта
+                        fontWeight: FontWeight.bold,
+                        color: Colors.orange,
+                        fontFamily: 'Roboto', // Выбрали шрифт Roboto
+                      ),
+                    ),
+                    const SizedBox(height: 20), // Отступ между заголовком и формой
+
+                    // Заголовок формы
+                    Text(
+                      _isSignIn ? 'Вход' : 'Регистрация',
+                      style: const TextStyle(
+                        fontSize: 32,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 30),
+
+                    // Поле логина
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: 'Почта',
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.2), // Используем withOpacity
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+
+                    // Поле пароля
+                    TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        hintText: 'Пароль',
+                        filled: true,
+                        fillColor: Colors.white.withOpacity(0.2), // Используем withOpacity
+                        border: const OutlineInputBorder(
+                          borderSide: BorderSide.none,
+                          borderRadius: BorderRadius.all(Radius.circular(20)),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+
+                    // Поле "Повторить пароль" (только для регистрации)
+                    if (!_isSignIn)
+                      TextField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                          hintText: 'Повторить пароль',
+                          filled: true,
+                          fillColor: Colors.white.withOpacity(0.2), // Используем withOpacity
+                          border: const OutlineInputBorder(
+                            borderSide: BorderSide.none,
+                            borderRadius: BorderRadius.all(Radius.circular(20)),
+                          ),
+                        ),
+                      ),
+                    const SizedBox(height: 30),
+
+                    // Кнопка "Войти"
+                    ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(context,
+                            MaterialPageRoute(
+                              builder: (context) => const MainPage()));
+                        // Обработка входа/регистрации
+                      },
+                      child: Text(_isSignIn ? 'Войти' : 'Зарегистрироваться'),
+                    ),
+                    const SizedBox(height: 20),
+
+                    // Кнопка "У меня еще нет аккаунта"
+                    TextButton(
+                      onPressed: () {
+                        setState(() {
+                          _isSignIn = !_isSignIn;
+                        });
+                      },
+                      child: Text(
+                        _isSignIn
+                            ? 'У меня еще нет аккаунта'
+                            : 'Вернуться на страницу входа',
+                        style: const TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -4,7 +4,7 @@ class TaskManagerTheme {
   static ThemeData lightTheme = ThemeData(
     // Используем colorScheme для определения цветов
     colorScheme: ColorScheme.fromSeed(
-      seedColor: const Color(0xFFffd389), // Primary color
+      seedColor: Colors.orange, 
       primary: const Color(0xFFffd389), // Primary color
       secondary: const Color(0xFF4ceb8b), // Secondary color
     ),
@@ -26,11 +26,23 @@ class TaskManagerTheme {
       showUnselectedLabels: true, // Показывать текст для неактивных иконок
       selectedLabelStyle: TextStyle(
         fontWeight: FontWeight.bold,
+        color: Colors.orange,
       ), // Стиль текста для выбранных иконок
       unselectedLabelStyle: TextStyle(
         fontWeight: FontWeight.normal,
       ), // Стиль текста для неактивных иконок
     ),
     // ...
+    // Добавляем тему для ElevatedButton
+    elevatedButtonTheme: ElevatedButtonThemeData(
+      style: ButtonStyle(
+        foregroundColor: MaterialStateProperty.all<Color>(Colors.orange), // Текст кнопок оранжевый,
+        textStyle: MaterialStateProperty.all<TextStyle>(
+          const TextStyle(
+            fontSize: 20,
+            color: Colors.orange), // Текст кнопок оранжевый
+      ),
+    ),
+  ),
   );
 }
